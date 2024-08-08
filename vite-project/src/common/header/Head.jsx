7694096c49b5764850a392273f48b229a540ce55
Ishwor Sapkota <isapkota777@gmail.com>
2024-08-08 17:28:13 +0545

    initial commit

---
+++ vite-project/src/common/header/Head.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Head = () => {
+  return (
+    <>
+      <section className="head">
+        <div className="container d_flex">
+          <div className="left row">
+            <i className="fa fa-phone"></i>
+            <label>+977 9823420516</label>
+            <i className="fa fa-envelope"></i>
+            <label>isapkota777@gmail.com</label>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Head;
